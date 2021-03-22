20aef978d8fde98f817931ebf3ad81131ed1a243
Eugene_Petrov <ipetrovgenya@gmail.com>
2021-03-22 15:53:25 +0300

    all done2

--- src/groups/views.py
+++ src/groups/views.py
@@ -132,7 +132,6 @@ class SeriesUpdate(UpdateView):
 #КНИГИ
 class BookList(ListView):
     model = Book
-    paginate_by = 10
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
